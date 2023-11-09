d753f0ec4fa85d61883ee92468e1fe2c2c8a76d2
PabloCT1998 <43761330+PabloCT1998@users.noreply.github.com>
2023-11-09 12:59:40 -0600

    Update crm.php

--- crm.php
+++ crm.php
@@ -7,10 +7,10 @@ require_once __DIR__ . '/vendor/autoload.php';
 use MicrosoftAzure\Storage\Blob\BlobRestProxy;
 use MicrosoftAzure\Storage\Common\Exceptions\ServiceException;
 
-    $fechaCreacion =date("d-m-Y-h-i-s");
-    $connectionString ='DefaultEndpointsProtocol=https;AccountName=azsolar;AccountKey=s1x0MhH7ErO+KCUzv1xImFcGbzfwO+ewifWmzaN43d6C/zqfO8LSOOCFNrTE10J31/pD5CQuIfuD+ASt68bhyA==;EndpointSuffix=core.windows.net';
-    $blobClient = BlobRestProxy::createBlobService($connectionString);
-    $blobService = new AzureBlobService($blobClient);
-    $containerName = 'azsolar';
+    //$fechaCreacion =date("d-m-Y-h-i-s");
+    ////$connectionString ='DefaultEndpointsProtocol=https;AccountName=azsolar;AccountKey=s1x0MhH7ErO+KCUzv1xImFcGbzfwO+ewifWmzaN43d6C/zqfO8LSOOCFNrTE10J31/pD5CQuIfuD+ASt68bhyA==;EndpointSuffix=core.windows.net';
+    //$blobClient = BlobRestProxy::createBlobService($connectionString);
+    //$blobService = new AzureBlobService($blobClient);
+    //$containerName = 'azsolar';
 echo "Hola"
 ?>
