54fa7098b4e915e655dd2a0ccb077e77b9267057
PabloCT1998 <43761330+PabloCT1998@users.noreply.github.com>
2023-11-09 12:47:22 -0600

    Update crm.php use

--- crm.php
+++ crm.php
@@ -3,5 +3,9 @@
 include('includes/conexion.php');
 include('includes/funciones.php');
 
+require_once __DIR__ . '/vendor/autoload.php';
+use MicrosoftAzure\Storage\Blob\BlobRestProxy;
+use MicrosoftAzure\Storage\Common\Exceptions\ServiceException;
+
 echo "Hola"
 ?>
