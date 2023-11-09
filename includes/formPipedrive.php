ae21af7edbe7fd6452a86fb04739aaa783946270
PabloCT1998 <43761330+PabloCT1998@users.noreply.github.com>
2023-11-09 11:21:31 -0600

    Update formPipedrive.php

--- includes/formPipedrive.php
+++ includes/formPipedrive.php
@@ -1,4 +1,4 @@
-<form action="guardarPipeDrive.php" method="POST" enctype="multipart/form-data">
+<form action="guardarCRM.php" method="POST" enctype="multipart/form-data">
                 
 			<?php if(isset($_SESSION['exitoCRM'])){?>
 				<div class="row">
