79262a104369c2ecc8d97454bd2e905945a4905f
PabloCT1998 <43761330+PabloCT1998@users.noreply.github.com>
2023-11-09 18:39:39 -0600

    Update resumenFinanciero.php

--- resumenFinanciero.php
+++ resumenFinanciero.php
@@ -1,4 +1,13 @@
 <?php 
 
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Headers: *");
+
+include('includes/conexion.php');
+include('includes/funciones.php');
+session_start();
+if (!definido($_SESSION['usuario'])) {
+    header('location: LoginUser.php');
+}
 echo 1;
 ?>
